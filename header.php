0b8a0f42dd3338e081e11f774d8944a4f1ae3b5f
Technolabs <45035044+Technolabs@users.noreply.github.com>
2018-11-14 12:39:38 +0100

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-		<!-- Mobile Meta -->
+		<!-- Mobile Meta truc much fghdfghdfghdfghdfg dfghdgfh dfghd fgh -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta class="foundation-mq">
 		<link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet">
